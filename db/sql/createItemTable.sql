18a4d25a9eabfe7e0bfddb218f35a87a283b20b2
Jeetjyoti Deka <jeetjyoti2020@gmail.com>
2025-01-12 13:23:30 +0530

    add images

--- db/sql/createItemTable.sql
+++ db/sql/createItemTable.sql
@@ -2,6 +2,7 @@ CREATE TABLE items (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     description TEXT,
+    img VARCHAR(50),
     price NUMERIC(10, 2) NOT NULL,
     stock INT NOT NULL DEFAULT 0,
     category_id INT REFERENCES categories(id) ON DELETE CASCADE,
